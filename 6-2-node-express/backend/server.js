83aafb638d0303afe0558dbe3d5c00cf01fb21d0
meznah <s202268000@kfupm.edu.sa>
2025-11-10 11:37:22 +0300

    TODO 4

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -4,15 +4,16 @@ import { getRandomQuote } from "./quotes.js";
 
 const app = express();
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+
 {/*write code for cors*/}
-
-
-
+app.use(cors());
 
 {/*write code to define routes*/}
 
-
 {/*write code to create server*/}
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
