9ca2fef7b0d3413a80b4e3063df480cbda7a96b9
meznah <s202268000@kfupm.edu.sa>
2025-11-10 11:47:44 +0300

    TODO 5

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -9,8 +9,15 @@ const PORT = 3000;
 app.use(cors());
 
 {/*write code to define routes*/}
+app.get("/", (req, res) => {
+    res.send("Welcome to the Quote Generator API");
+});
 
 {/*write code to create server*/}
+app.get("/api/quote", (req, res) => {
+    const quote = getRandomQuote();
+    res.json({ quote });
+});
 
 
 
